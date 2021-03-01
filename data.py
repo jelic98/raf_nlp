46f0d4dd3b6d16dca35d55d1e5dc829bb59526bc
Lazar Jelic <lazarjelic98@gmail.com>
2021-03-01 21:00:33 +0100

    Merged data.sh and stop.sh

--- data.py
+++ data.py
@@ -1,9 +1,18 @@
 import gdown
 import argparse
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('url')
 arg_parser.add_argument('file_d_out')
+arg_parser.add_argument('file_s_out')
 args = arg_parser.parse_args()
 
 gdown.download(args.url, args.file_d_out, quiet=False)
+
+with open(args.file_s_out, 'w') as file_s_out:
+    for word in set(stopwords.words('english')):
+        file_s_out.write(word + '\n')
--- data.sh
+++ data.sh
@@ -2,10 +2,19 @@
 
 # Declare path constants
 readonly DIR=.
+readonly DDIR=./data
 readonly D=$DIR/data.zip
-readonly URL=https://drive.google.com/uc?id=1fGYzEkGSTLBDFSRRiQw9jeaC9xHWws2g
+readonly S=$DDIR/nltk_stop_words.txt
+readonly URL=https://drive.google.com/uc?id=1EP6A8k4pOssRP4bNKmrrT3dGQnPBhfdf
 
 echo "[DATA] Download dataset"
 pip3 install gdown
 python3 data.py $URL $D
-unzip $D && rm $D
+unzip $D && rm $D && rm -rf __MACOSX/
+
+echo "[DATA] Download NLTK stop words"
+pip3 install nltk
+python3 stop.py $S
+sed 's/[^a-z]//g' $S > $S.sed
+sort $S.sed > $S
+rm $S.sed
