22945036b5a4e649d9808c7a944d93c69d4f4e07
Lazar Jelic <lazarjelic98@gmail.com>
2021-03-01 21:14:41 +0100

    Updated main.c

--- main.c
+++ main.c
@@ -6,17 +6,12 @@
 dt_char arg_train[PATH_CHARACTER_MAX] = {0};
 dt_char arg_test[PATH_CHARACTER_MAX] = {0};
 dt_char arg_stop[PATH_CHARACTER_MAX] = {0};
-dt_char arg_out[PATH_CHARACTER_MAX] = {0};
 
 int main(int argc, char* argv[]) {	
 	if(argc == 4) {
 		strcpy(arg_train, argv[1]);
 		strcpy(arg_test, argv[2]);
 		strcpy(arg_stop, argv[3]);
-
-		printf("1:%s[END]\n", argv[1]);
-		printf("2:%s[END]\n", argv[2]);
-		printf("3:%s[END]\n", argv[3]);
 	}else {
 #ifdef FLAG_LOG
 		echo_fail(ERROR_CMDARGS);
