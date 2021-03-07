495cd937e5469f897090a7fae06f00dfdaaac174
Lazar Jelic <lazarjelic98@gmail.com>
2021-03-07 23:51:09 +0100

    Cleaning worksapve before bash script

--- data.sh
+++ data.sh
@@ -8,6 +8,9 @@ readonly D=$DIR/data.zip
 readonly S=$DDIR/nltk_stop_words.txt
 readonly URL=https://drive.google.com/uc?id=1EP6A8k4pOssRP4bNKmrrT3dGQnPBhfdf
 
+echo "[$(eval $DATE)] [PIPELINE] Clean workspace"
+rm -f $S
+
 echo "[$(eval $DATE)] [DATA] Download libraries"
 python3 -m pip install --upgrade pip
 pip3 install gdown
@@ -19,6 +22,4 @@ python3 data.py $URL $D $S
 unzip $D && rm $D
 sed 's/[^a-z]//g' $S > $S.sed
 sort $S.sed > $S
-rm $S.sed
-rm .DS_Store
-rm -rf __MACOSX/
+rm -rf __MACOSX/ .DS_Store $S.sed
--- pipeline.sh
+++ pipeline.sh
@@ -4,18 +4,23 @@
 readonly DATE="date +'%d-%m-%Y %H:%M:%S'"
 readonly DDIR=./data
 readonly ODIR=./out
-readonly QA=$DDIR/xz/small_segmented_questions_and_answers
+readonly QA=$DDIR/xz/segmented_questions_and_answers
 readonly Q=$DDIR/questions
 readonly A=$DDIR/answers
 readonly S=$DDIR/nltk_stop_words.txt
 readonly V=$ODIR/vocab.tsv
 readonly W=$ODIR/weights-ih.tsv
 
+printf "\033[0;36m"
+
+echo "[$(eval $DATE)] [PIPELINE] Clean workspace"
+rm -f $QA.enc.xz $QA.enc $QA.low $QA $Q.fil $Q $A $V $W
+
 echo "[$(eval $DATE)] [PIPELINE] Decmopress questions and answers"
 xz -dkf -T0 $QA.xz
 
 echo "[$(eval $DATE)] [PIPELINE] Format questions and answers"
-tr '[:upper:]' '[:lower:]' < $QA > $QA.new && mv $QA.new $QA
+tr '[:upper:]' '[:lower:]' < $QA > $QA.low && mv $QA.low $QA
 perl -pi -e 's/[^\w\s|\b=~=~>\b]//g' $QA
 for c in {a..z}; do perl -pi -e 's/'$c'{3,}/'$c$c'/g' $QA; done
 
@@ -24,7 +29,7 @@ grep -E -o '^.* =~=~>' $QA > $Q
 perl -pi -e 's/ =~=~>//g' $Q
 grep -E -o '=~=~>.*$' $QA > $A
 perl -pi -e 's/=~=~> //g' $A
-rm $QA
+rm -f $QA
 
 echo "[$(eval $DATE)] [PIPELINE] Filter questions"
 python3 filter.py $Q $Q.fil 3 0.001
@@ -36,7 +41,9 @@ make ARG_TRAIN="$Q" ARG_TEST="/dev/null" ARG_STOP="$S"
 
 echo "[$(eval $DATE)] [PIPELINE] Encode questions"
 python3 encoder.py $Q $A $QA.enc $V $W
-rm $Q $A
+rm -f $Q $A
 
 echo "[$(eval $DATE)] [PIPELINE] Compress encoded questions and answers"
 xz -zf -T0 -0 $QA.enc
+
+printf "\033[0m"
