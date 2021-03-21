7f86628a0466741c39639c42e2cf986daf6d9de7
Lazar Jelic <lazarjelic98@gmail.com>
2021-03-21 10:49:17 +0100

    Updated hyperparameters

--- demo.sh
+++ demo.sh
@@ -2,6 +2,6 @@
 
 make clean
 make	ARG_ACTIONS="LOAD,TRAIN,SAVE" \
-		ARG_TRAIN="data/train/bill_gates.txt" \
-		ARG_TEST="data/test/bill_gates.txt" \
+		ARG_TRAIN="data/train/text8.txt" \
+		ARG_TEST="data/test/text8.txt" \
 		ARG_STOP="data/nltk_stop_words.txt"
